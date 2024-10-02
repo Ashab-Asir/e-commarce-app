f0cd4cb6baa1e8e45e043c46843b08cb1c75a022
Ashab-Asir <ashabasir101@gmail.com>
2024-10-02 08:11:10 +0600

    loading state added

--- client/src/components/product/ProductGrid.jsx
+++ client/src/components/product/ProductGrid.jsx
@@ -5,13 +5,17 @@ import { ProductServices } from "../../api/services";
 
 export default function ProductGrid() {
   const [products, setProducts] = useState([]);
+  const [isLoading, setLoading] = useState(false);
   const loadProducts = async () => {
     try {
+      setLoading(true);
       const Products = await ProductServices.getProducts();
       setProducts(Products);
     } catch (err) {
       alert("Failed to load products");
       console.error(err);
+    } finally {
+      setLoading(false);
     }
   };
   useEffect(() => {
@@ -19,9 +23,13 @@ export default function ProductGrid() {
   }, []);
   return (
     <div className="productGrid">
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product}></ProductCard>
-      ))}
+      {isLoading ? (
+        <h1>Loading</h1>
+      ) : (
+        products.map((product) => (
+          <ProductCard key={product.id} product={product}></ProductCard>
+        ))
+      )}
     </div>
   );
 }
